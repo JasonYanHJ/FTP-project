2ddaa8d0956c8e2e48adec695da69cf1faa75c94
LinYiCheng <yichenglin20@fudan.edu.cn>
2022-11-18 16:44:15 +0800

    Add file_io_helper.h/.c

---
+++ common/file_io_helper.c
@@ -0,0 +1,5 @@
+//
+// Created by Ethan on 2022/11/18.
+//
+
+#include "file_io_helper.h"
---
+++ common/file_io_helper.h
@@ -0,0 +1,12 @@
+//
+// Created by Ethan on 2022/11/18.
+//
+
+#ifndef PROJECT_FILE_IO_HELPER_H
+#define PROJECT_FILE_IO_HELPER_H
+
+#define _for_in_fp(N, buffer, read_cnt, fp) char buffer[N+1]; \
+ for(size_t read_cnt = 0; (read_cnt = fread(&buffer, sizeof(char), N, fp))!=0;)
+
+
+#endif //PROJECT_FILE_IO_HELPER_H
