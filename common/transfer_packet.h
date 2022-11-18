8bd884a3ac649bb80dfb65e17f5295b7db0ceaec
LinYiCheng <yichenglin20@fudan.edu.cn>
2022-11-18 20:19:23 +0800

    Add transfer_packet.h

---
+++ common/transfer_packet.h
@@ -0,0 +1,15 @@
+//
+// Created by Ethan on 2022/11/18.
+//
+
+#ifndef PROJECT_TRANSFER_PACKET_H
+#define PROJECT_TRANSFER_PACKET_H
+
+// #define BUF_SIZE 256
+
+//struct Packet{
+//    unsigned int buffer_size;
+//    char buffer[BUF_SIZE];
+//};
+
+#endif //PROJECT_TRANSFER_PACKET_H
